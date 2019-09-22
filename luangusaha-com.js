1107023bca92de73ea3e524eaa07fcf4f4a521e3
anggista <55666072+anggista@users.noreply.github.com>
2019-09-23 04:39:07 +0700

    Update luangusaha-com.js

--- luangusaha-com.js
+++ luangusaha-com.js
@@ -816,9 +816,4 @@ $(function () {
     })
 })
 
-var images = document.getElementsByTagName('img');
-  for (var i = 0; i < images.length; i++) {
-    images[i].src = images[i].src.replace(/.*?:\/\//g ,
-"https://cdn.staticaly.com/img/");
-  };
 //]]>
